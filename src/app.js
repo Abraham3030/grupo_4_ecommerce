9c52a13521f09985a1c89fb0bc0eb4bf6ff14260
Abraham Montelongo <mocapunk@hotmail.com>
2021-06-02 03:28:03 -0500

    Cree mis dos vistas en una carpeta provicional en public, asi como varias modificaciones al proyecto son faciles de ver

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path    = require('path');
+
 const app = express();
-const path = require('path');
-const publicPath = path.join(__dirname, './public');
+const publicPath = path.resolve(__dirname, './public');
 const PORT = 3000;
 
-app.use(express.static(publicPath));
+app.use( express.static( publicPath ));
 
-app.listen(PORT, () => {
+app.listen( PORT, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home/index.html'));
+app.get('/', ( req, res ) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
-
-
-

1bcb9bbc46332994e4fbacb7ce57441f5163cc8f
Abraham Montelongo <mocapunk@hotmail.com>
2021-06-17 00:10:54 -0500

    modificacion nodemon y app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path    = require('path');
-
 const app = express();
+const PORT      = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, './public');
-
-
 app.use( express.static( publicPath ));
-
-app.listen( 3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-});
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
@@ -33,4 +27,8 @@ app.post('/productCart', (req,res) => {
 
 app.get('/productDetail', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
-})
+});
+
+app.listen( PORT, () => {
+    console.log('Servidor corriendo en el puerto 3000', PORT);
+});
